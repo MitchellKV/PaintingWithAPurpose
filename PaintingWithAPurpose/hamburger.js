054b44ab42146264e29544821d41ad7aceb9b97a
mitchellkv-uwec <valentmk9033@uwec.edu>
2023-05-18 18:41:19 -0500

    PaintingWithAPurpose Complete

--- PaintingWithAPurpose/hamburger.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var hamburgerToggle = document.getElementById('hamburger-toggle');
-    var menu = document.querySelector('.menu');
-  
-    hamburgerToggle.addEventListener('change', function() {
-      menu.classList.toggle('active');
-    });
-  });
